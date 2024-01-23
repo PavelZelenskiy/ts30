4c84fb7e2671dbe543e389f0aaaef016a919640f
Pavel Zelenskiy <p.z.box@mail.ru>
2024-01-23 12:43:48 +0300

    refactoring

--- src/components/Clock/TsClock.tsx
+++ src/components/Clock/TsClock.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./tsClock.css";
 
 export function TsClock() {
-  return <div className="ts_clock">TS Clock</div>;
+  return <div className="ts_clock">Здесь будет TS Clock</div>;
 }
