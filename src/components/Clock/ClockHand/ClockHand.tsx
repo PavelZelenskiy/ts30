9bb116c57dcc6193dbe435bf237c3fa457ddd11b
Pavel Zelenskiy <p.z.box@mail.ru>
2024-01-26 21:38:13 +0300

    add TSClock project

---
+++ src/components/Clock/ClockHand/ClockHand.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import "./clockHand.css";
+
+type timeType = {
+  timeType: string;
+};
+
+export function ClockHand({ timeType }: timeType) {
+  const [time, setTime] = useState(0);
+
+  let handHeight: string;
+  let handWidth: string;
+  let handLeft: string;
+  let transition: string;
+
+  function updateTime(timeType: string) {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    if (timeType === "minutes") {
+      setTime((minutes / 60) * 360 + 90);
+    } else if (timeType === "hours") {
+      setTime((hours / 12) * 360 + 90);
+    } else {
+      setTime((seconds / 60) * 360 + 90);
+    }
+  }
+
+  if (timeType === "minutes") {
+    handHeight = "4px";
+    handWidth = "40%";
+    handLeft = "10%";
+    transition = "all 0.05s";
+  } else if (timeType === "hours") {
+    handHeight = "6px";
+    handWidth = "25%";
+    handLeft = "25%";
+    transition = "all 0.05s";
+  } else {
+    handHeight = "2px";
+    handWidth = "50%";
+    handLeft = "0";
+    transition = "";
+  }
+
+  setInterval(() => updateTime(timeType), 1000);
+
+  type HandStyle = {
+    width: string;
+    height: string;
+    background: string;
+    position: any;
+    top: string;
+    left: string;
+    transformOrigin: string;
+    transition: string;
+    transitionTimingFunction: string;
+    transform: string;
+  };
+
+  const handStyle: HandStyle = {
+    width: handWidth,
+    height: handHeight,
+    background: "black",
+    position: "absolute",
+    top: "50%",
+    left: handLeft,
+    transformOrigin: "100%",
+    transition: transition,
+    transitionTimingFunction: "cubic-bezier(0, 2.21, 0.58, 1)",
+    transform: `rotate(${time}deg)`,
+  };
+
+  return (
+    <>
+      <div style={handStyle}></div>
+    </>
+  );
+}
--- src/components/Clock/TsClock.tsx
+++ src/components/Clock/TsClock.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import "./tsClock.css";
+import { ClockHand } from "./ClockHand/ClockHand";
 
 export function TsClock() {
-  return <div className="ts_clock">TS Clock</div>;
+  return (
+    <div className="ts_clock">
+      <div className="clock_face">
+        <div className="clock_face_inner">
+          <ClockHand timeType="seconds" />
+          <ClockHand timeType="minutes" />
+          <ClockHand timeType="hours" />
+        </div>
+      </div>
+    </div>
+  );
 }
